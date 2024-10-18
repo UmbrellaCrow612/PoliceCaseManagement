3d9117c5d8d9adb3ec00e6db1194ae6604291740
Yousaf Wazir <umbrellaman612@gmail.com>
2024-10-18 09:27:49 +0100

    ICrimeSceneRepository

---
+++ src/PoliceCaseManagement.Core/Interfaces/ICrimeSceneRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PoliceCaseManagement.Core.Interfaces
+{
+    public interface ICrimeSceneRepository<T, TId> : IGenericRepository<T, TId> where T : class
+    {
+        /// <summary>
+        /// Search for crime scenes based on specified criteria.
+        /// </summary>
+        /// <returns>A collection of crime scenes that match the search criteria.</returns>
+        Task<IEnumerable<T>> SearchAsync();
+    }
+}
---
+++ src/PoliceCaseManagement.Infrastructure/Data/Repositories/CrimeSceneRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PoliceCaseManagement.Core.Entities;
+using PoliceCaseManagement.Core.Interfaces;
+
+namespace PoliceCaseManagement.Infrastructure.Data.Repositories
+{
+    public class CrimeSceneRepository(ApplicationDbContext context) : ICrimeSceneRepository<CrimeScene, string>
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task AddAsync(CrimeScene entity)
+        {
+            await _context.CrimeScenes.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(CrimeScene entity)
+        {
+            var crimeSceneToDelete = await _context.CrimeScenes.FirstOrDefaultAsync(x => x.Id == entity.Id);
+            if (crimeSceneToDelete is null) return false;
+
+            _context.CrimeScenes.Remove(crimeSceneToDelete);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<CrimeScene?> GetByIdAsync(string id)
+        {
+            var crimeSceneToGet = await _context.CrimeScenes.FirstOrDefaultAsync(x => x.Id == id);
+            if (crimeSceneToGet is null) return null;
+
+            return crimeSceneToGet;
+        }
+
+        public Task<IEnumerable<CrimeScene>> SearchAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateAsync(CrimeScene entity)
+        {
+            var crimeSceneToUpdate = await _context.CrimeScenes.FirstOrDefaultAsync(x => x.Id == entity.Id);
+            if (crimeSceneToUpdate is null) return false;
+
+            _context.Entry(crimeSceneToUpdate).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
