62d9d1de4035949b71ca49fbd0368f2b3367a911
Yousaf Wazir <umbrellaman612@gmail.com>
2024-10-22 16:00:51 +0100

    basic case CRUD endpoints

---
+++ src/PoliceCaseManagement.Api/Controllers/CaseController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoliceCaseManagement.Application.DTOs.Cases;
+using PoliceCaseManagement.Application.Interfaces;
+
+namespace PoliceCaseManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("cases")]
+    public class CaseController(ICaseService caseService) : ControllerBase
+    {
+        private readonly ICaseService _caseService = caseService;
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CaseDto>> GetCaseById(string id)
+        {
+            var caseToGet = await _caseService.GetCaseByIdAsync(id);
+            if(caseToGet is null) return NotFound("Case not found.");
+
+            return caseToGet;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CaseDto>> CreateCase([FromBody] CreateCaseDto request)
+        {
+            string userId = "";
+
+            var createdCase = await _caseService.CreateCaseAsync(userId, request);
+
+            return createdCase;
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> UpdateCase(string id, [FromBody] UpdateCaseDto request)
+        {
+            var succeeded = await _caseService.UpdateCaseAsync(id, request);
+            if (!succeeded) return NotFound("Case not found.");
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCase(string id)
+        {
+            var succeeded = await _caseService.DeleteCaseAsync(id);
+            if (!succeeded) return NotFound("Case not found ");
+
+            return NoContent();
+        }
+    }
+}
--- src/PoliceCaseManagement.Application/DTOs/Cases/CreateCaseDto.cs
+++ src/PoliceCaseManagement.Application/DTOs/Cases/CreateCaseDto.cs
@@ -1,12 +1,16 @@
 ﻿using PoliceCaseManagement.Core.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace PoliceCaseManagement.Application.DTOs.Cases
 {
     public class CreateCaseDto
     {
+        [Required]
+        [EnumDataType(typeof(CasePriority))]
         public required CasePriority CasePriority { get; set; }
+        [Required]
         public required string Title { get; set; }
+        [Required]
         public required string Description { get; set; }
-        public required string CreatedById { get; set; }
     }
 }
