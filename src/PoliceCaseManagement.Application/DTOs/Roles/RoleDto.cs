92f5197fcb88ce4efda95cd419b6cdb7dff5cfde
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-03 10:09:47 +0000

    removed role dtos

--- src/PoliceCaseManagement.Application/DTOs/Roles/RoleDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PoliceCaseManagement.Application.DTOs.Roles
-{
-    public class RoleDto
-    {
-        public required string Id { get; set; }
-        public required string Name { get; set; }
-    }
-}
