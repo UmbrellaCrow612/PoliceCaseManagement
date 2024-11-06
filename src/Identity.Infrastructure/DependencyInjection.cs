e601cac8eebaf42f8b4ee02b3b10d7d13084e87b
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-06 13:07:27 +0000

    Update DependencyInjection.cs

--- src/Identity.Infrastructure/DependencyInjection.cs
+++ src/Identity.Infrastructure/DependencyInjection.cs
@@ -7,10 +7,21 @@ namespace Identity.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException(
+                    "Connection string 'DefaultConnection' not found.");
+            }
+
             services.AddDbContext<IdentityApplicationDbContext>(options =>
-                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(connectionString,
+                    b => b.MigrationsAssembly(typeof(IdentityApplicationDbContext).Assembly.FullName)));
 
             return services;
         }
