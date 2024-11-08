454c85d0051ced05b80ee33bb205967158dea6c7
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-08 14:53:23 +0000

    Update AuthenticationController.cs

--- src/Identity.API/Controllers/AuthenticationController.cs
+++ src/Identity.API/Controllers/AuthenticationController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Identity.Api.Jwt;
+using Identity.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Api.Controllers
 {
     [ApiController]
     [Route("auth")]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(JwtService jwtService, UserManager<ApplicationUser> userManager, IUserStore<ApplicationUser> userStore, RoleManager<IdentityRole> roleManager) : ControllerBase
     {
+        private readonly JwtService _jwtService = jwtService;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly IUserStore<ApplicationUser> _userStore = userStore;
+        private readonly IUserEmailStore<ApplicationUser> _userEmailStore = (IUserEmailStore<ApplicationUser>)userStore;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
         [HttpPost]
         public async Task<ActionResult> Login()
         {
