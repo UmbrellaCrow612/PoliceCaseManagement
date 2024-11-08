d5a94bff04c681855bb7ded2753a3ca7c35302b7
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-08 16:47:27 +0000

    LoginDto

--- src/Identity.API/Controllers/AuthenticationController.cs
+++ src/Identity.API/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using Identity.Api.Jwt;
+﻿using Identity.Api.DTOs;
+using Identity.Api.Jwt;
 using Identity.Core.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,9 +16,30 @@ namespace Identity.Api.Controllers
         private readonly IUserEmailStore<ApplicationUser> _userEmailStore = (IUserEmailStore<ApplicationUser>)userStore;
         private readonly RoleManager<IdentityRole> _roleManager = roleManager;
 
-        [HttpPost]
-        public async Task<ActionResult> Login()
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
         {
+            if (string.IsNullOrEmpty(loginDto.Email) && string.IsNullOrEmpty(loginDto.Username)) return BadRequest("Must provide either a username or email in login request.");
+            if (loginDto.Email is not null && loginDto.Username is not null) return BadRequest("Do not provide both username and email just one.");
+
+            // Flow for email
+            if (loginDto.Email is not null)
+            {
+                var user = await _userManager.FindByEmailAsync(loginDto.Email);
+                if (user is null) return NotFound("User not found.");
+
+                // Generate claims and write token out.
+            }
+
+            // Flow for username
+            if (loginDto.Username is not null)
+            {
+                var user = await _userStore.FindByNameAsync(loginDto.Username, CancellationToken.None);
+                if (user is null) return NotFound("User not found.");
+
+                // Generate claims and write token
+            }
+
             return Ok();
         }
     }
---
+++ src/Identity.API/DTOs/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Api.DTOs
+{
+    /// <summary>
+    /// Login in details for identity - provide either a username or email and a password.
+    /// </summary>
+    public class LoginDto
+    {
+        [EmailAddress]
+        public string? Email { get; set; } = null;
+
+        public string? Username { get; set; } = null;
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
