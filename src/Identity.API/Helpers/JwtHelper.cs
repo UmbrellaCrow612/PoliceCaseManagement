46379df6e98291ac206bee027041ae6e8803f979
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-09 19:04:58 +0000

    Create JwtHelper.cs

---
+++ src/Identity.API/Helpers/JwtHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Identity.Api.Helpers
+{
+    public class JwtHelper(IConfiguration configuration)
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"] ?? throw new ApplicationException("JWT Key not provided in config.")));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claimsList = claims.ToList();
+            claimsList.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claimsList.Add(new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString()));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"] ?? throw new ApplicationException("JWT Issuer not provided in config."),
+                audience: _configuration["JWT:Audience"] ?? throw new ApplicationException("JWT Audience not provided in config."),
+                claims: claimsList,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
