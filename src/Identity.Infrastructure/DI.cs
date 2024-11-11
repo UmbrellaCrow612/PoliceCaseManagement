304aeaeb5ed2cc86e20ddda9a2f28fd63f1f685f
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-11 10:30:20 +0000

    added Db Context

---
+++ src/Identity.Infrastructure/DI.cs
@@ -0,0 +1,26 @@
+﻿using Identity.Core;
+using Identity.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Infrastructure
+{
+    public static class DI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DefaultConnection"] ?? throw new ArgumentException("connectionString is empty");
+
+            services.AddDbContext<IdentityApplicationDbContext>(
+                options => options.UseSqlite(connectionString));
+
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<IdentityApplicationDbContext>()
+                .AddRoles<IdentityRole>();
+         
+            return services;
+        }
+    }
+}
---
+++ src/Identity.Infrastructure/Data/IdentityApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Core;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Data
+{
+    public class IdentityApplicationDbContext(DbContextOptions<IdentityApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
