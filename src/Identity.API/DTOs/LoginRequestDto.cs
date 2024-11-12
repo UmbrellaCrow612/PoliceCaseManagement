1efad3871fc3b91a61766ef5d995f32db6bce928
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-12 14:10:27 +0000

    Some more Data Annotations

--- src/Identity.API/DTOs/LoginRequestDto.cs
+++ src/Identity.API/DTOs/LoginRequestDto.cs
@@ -4,8 +4,10 @@ namespace Identity.API.DTOs
 {
     public class LoginRequestDto
     {
+        [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot contain only whitespace.")]
         public string? UserName { get; set; } = null;
 
+        [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot contain only whitespace.")]
         [EmailAddress]
         public string? Email { get; set; } = null;
 
