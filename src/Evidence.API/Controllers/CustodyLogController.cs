706fc7b19100a23186449d042d866b0bd15d66d5
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-19 09:31:36 +0000

    custody-logs endpoints added

--- src/Evidence.API/Controllers/CustodyLogController.cs
+++ src/Evidence.API/Controllers/CustodyLogController.cs
@@ -1,33 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Evidence.API.DTOs;
+using Evidence.Infrastructure.Data.Stores;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Evidence.API.Controllers
 {
     [ApiController]
     [Route("evidence/{evidenceId}/custody-logs")]
-    public class CustodyLogController : ControllerBase
+    public class CustodyLogController(ICustodyLogStore custodyLogStore , IEvidenceItemStore evidenceItemStore, IMapper mapper) : ControllerBase
     {
+        private readonly ICustodyLogStore _custodyLogStore = custodyLogStore;
+        private readonly IEvidenceItemStore _evidenceItemStore = evidenceItemStore;
+        private readonly IMapper _mapper = mapper;
+
         [HttpPost]
-        public async Task<ActionResult> CreateCustodyLog()
+        public async Task<ActionResult> CreateCustodyLog(string evidenceId)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found");
+
+            return Created();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCustodyLogs(string evidenceId)
+        {
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found");
+
+            var logs = await _custodyLogStore.GetCustodyLogs(evidence);
+
+            var dto = _mapper.Map<IEnumerable<CustodyLogDto>>(logs);
+
+            return Ok(dto);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult> GetCustodyLogById()
+        public async Task<ActionResult> GetCustodyLogById(string evidenceId, string id)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found");
+
+            var log = await _custodyLogStore.GetCustodyLogById(evidence, id);
+            if(log is null) return NotFound("Custody log not found.");
+
+            var dto = _mapper.Map<CustodyLogDto>(log);
+
+            return Ok(dto);
         }
 
         [HttpPatch("{id}")]
-        public async Task<ActionResult> UpdateCustodyLogById()
+        public async Task<ActionResult> UpdateCustodyLogById([FromBody] UpdateCustodyLogDto updateCustodyLogDto, string id, string evidenceId)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found");
+
+            var log = await _custodyLogStore.GetCustodyLogById(evidence, id);
+            if (log is null) return NotFound("Custody log not found.");
+
+            _mapper.Map(updateCustodyLogDto, log);
+
+            await _custodyLogStore.UpdateCustodyLog(evidence, log);
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteCustodyLogById()
+        public async Task<ActionResult> DeleteCustodyLogById(string id, string evidenceId)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found");
+
+            var log = await _custodyLogStore.GetCustodyLogById(evidence, id);
+            if (log is null) return NotFound("Custody log not found.");
+
+            await _custodyLogStore.DeleteCustodyLog(evidence, log);
+
+            return NoContent();
         }
     }
 }
--- src/Evidence.Infrastructure/Data/Stores/ICustodyLogStore.cs
+++ src/Evidence.Infrastructure/Data/Stores/ICustodyLogStore.cs
@@ -5,8 +5,9 @@ namespace Evidence.Infrastructure.Data.Stores
     public interface ICustodyLogStore
     {
         Task<(bool Succeeded, IEnumerable<string> Errors)> CreateCustodyLog(CustodyLog custodyLog, EvidenceItem evidence);
-        Task DeleteCustodyLog(CustodyLog custodyLog);
-        Task<CustodyLog?> GetCustodyLogById (string id);
-        Task UpdateCustodyLog(CustodyLog custodyLog);
+        Task DeleteCustodyLog(EvidenceItem evidence, CustodyLog custodyLog);
+        Task<CustodyLog?> GetCustodyLogById (EvidenceItem evidence, string id);
+        Task<IEnumerable<CustodyLog>> GetCustodyLogs(EvidenceItem evidence);
+        Task UpdateCustodyLog(EvidenceItem evidence, CustodyLog custodyLog);
     }
 }
