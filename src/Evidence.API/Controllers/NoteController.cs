1bafb5398739ae63601de31cb323d69637259f9c
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-19 11:35:24 +0000

    NoteController

--- src/Evidence.API/Controllers/NoteController.cs
+++ src/Evidence.API/Controllers/NoteController.cs
@@ -1,33 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Evidence.API.DTOs;
+using Evidence.Infrastructure.Data.Models;
+using Evidence.Infrastructure.Data.Stores;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Evidence.API.Controllers
 {
     [ApiController]
     [Route("evidence/{evidenceId}/notes")]
-    public class NoteController : ControllerBase
+    public class NoteController(INoteStore noteStore, IEvidenceItemStore evidenceItemStore, IMapper mapper) : ControllerBase
     {
+        private readonly IMapper _mapper = mapper;
+        private readonly INoteStore _noteStore = noteStore;
+        private readonly IEvidenceItemStore _evidenceItemStore = evidenceItemStore;
+
         [HttpPost]
-        public async Task<ActionResult> CreateNote()
+        public async Task<ActionResult> CreateNote(string evidenceId, [FromBody] CreateNoteDto createNoteDto)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found.");
+
+            var note = _mapper.Map<Note>(createNoteDto);
+
+            var (Succeeded, Errors) = await _noteStore.CreateNote(evidence, note);
+            if (!Succeeded) return BadRequest(Errors);
+
+            return Created();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetNotes(string evidenceId)
+        {
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found.");
+
+            var notes = await _noteStore.GetNotes(evidence);
+
+            var dto = _mapper.Map<IEnumerable<NoteDto>>(notes);
+
+            return Ok(dto);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult> GetNoteById()
+        public async Task<ActionResult> GetNoteById(string id, string evidenceId)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found.");
+
+            var note = await _noteStore.GetNoteById(evidence, id);
+
+            var dto = _mapper.Map<NoteDto>(note);
+
+            return Ok(dto);
         }
 
         [HttpPatch("{id}")]
-        public async Task<ActionResult> UpdateNoteById()
+        public async Task<ActionResult> UpdateNoteById(string id, string evidenceId, [FromBody] UpdateNoteDto updateNoteDto)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found.");
+
+            var note = await _noteStore.GetNoteById(evidence, id);
+
+            _mapper.Map(updateNoteDto, note);
+
+            await _noteStore.UpdateNote(evidence, note);
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteNoteById()
+        public async Task<ActionResult> DeleteNoteById(string id, string evidenceId)
         {
-            return Ok();
+            var evidence = await _evidenceItemStore.GetEvidenceById(evidenceId);
+            if (evidence is null) return NotFound("Evidence not found.");
+
+            var note = await _noteStore.GetNoteById(evidence, id);
+
+            await _noteStore.DeleteNote(evidence, note);
+
+            return NoContent();
         }
     }
 }
--- src/Evidence.Infrastructure/Data/Stores/INoteStore.cs
+++ src/Evidence.Infrastructure/Data/Stores/INoteStore.cs
@@ -1,6 +1,13 @@
-﻿namespace Evidence.Infrastructure.Data.Stores
+﻿using Evidence.Infrastructure.Data.Models;
+
+namespace Evidence.Infrastructure.Data.Stores
 {
     public interface INoteStore
     {
+        Task<(bool Succeeded, IEnumerable<string> Errors)> CreateNote(EvidenceItem evidence, Note note);
+        Task<IEnumerable<Note>> GetNotes(EvidenceItem evidence);
+        Task<Note> GetNoteById(EvidenceItem evidence, string id);
+        Task UpdateNote(EvidenceItem evidence, Note note);
+        Task DeleteNote(EvidenceItem evidence, Note note);
     }
 }
