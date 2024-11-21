d59e79803c9550136d70a778d8217128b0091a88
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-21 13:46:32 +0000

    user endpoints

--- src/Identity.API/Controllers/UserController.cs
+++ src/Identity.API/Controllers/UserController.cs
@@ -1,6 +1,39 @@
-﻿namespace Identity.API.Controllers
+﻿using Identity.API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
     {
+        [Authorize]
+        [HttpGet("{userId}")]
+        public async Task<ActionResult> GetUserById(string userId)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> SearchUsers([FromQuery] SearchUsersQuery query)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPatch("{userId}")]
+        public async Task<ActionResult> PatchUserById()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult> DeleteUserById(string userId)
+        {
+            return Ok();
+        }
     }
 }
---
+++ src/Identity.API/DTOs/SearchUsersQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.API.DTOs
+{
+    public class SearchUsersQuery
+    {
+        public string? UserName { get; set; } = null;
+    }
+}
