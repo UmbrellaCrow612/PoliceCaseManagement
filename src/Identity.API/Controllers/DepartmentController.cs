61ce06da4427b03845e49f77029f62fb05a7c29b
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-22 17:38:35 +0000

    Update DepartmentController.cs

--- src/Identity.API/Controllers/DepartmentController.cs
+++ src/Identity.API/Controllers/DepartmentController.cs
@@ -1,13 +1,20 @@
 ﻿using Identity.API.DTOs;
+using Identity.Infrastructure.Data.Models;
+using Identity.Infrastructure.Data.Stores;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.API.Controllers
 {
     [ApiController]
     [Route("departments")]
-    public class DepartmentController : ControllerBase
+    public class DepartmentController(IDepartmentStore departmentStore, UserManager<ApplicationUser> userManager, ILogger<DepartmentController> logger) : ControllerBase
     {
+        private readonly IDepartmentStore _departmentStore = departmentStore;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly ILogger<DepartmentController> _logger = logger;
+
         [Authorize]
         [HttpPost]
         public async Task<ActionResult> CreateDepartment()
@@ -19,8 +26,22 @@ namespace Identity.API.Controllers
         [HttpPost("{departmentId}/users/{userId}")]
         public async Task<ActionResult> AddUserToDepartment(string departmentId, string userId)
         {
-            // assign one user
-            return Ok();
+            var department = await _departmentStore.GetDepartmentById(departmentId);
+            if(department is null) return NotFound("Department not found.");
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user is null) return NotFound("User not found.");
+
+            if (await _departmentStore.IsUserInDepartment(department, user))
+            {
+                return BadRequest("User already in department");
+            }
+
+            await _departmentStore.AddUser(department, user);
+
+            _logger.LogInformation("User {id} added to department {depId}", user.Id, department.Id);
+
+            return NoContent();
         }
 
         [Authorize]
