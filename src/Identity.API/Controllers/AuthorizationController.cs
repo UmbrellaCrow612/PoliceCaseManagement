1d6ebe93dbea35b85a849fda8a83048f5222b798
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-22 15:46:26 +0000

    UnAssignUserRoles

--- src/Identity.API/Controllers/AuthorizationController.cs
+++ src/Identity.API/Controllers/AuthorizationController.cs
@@ -3,6 +3,7 @@ using Identity.Infrastructure.Data.Stores;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace Identity.API.Controllers
@@ -60,23 +61,65 @@ namespace Identity.API.Controllers
             return NoContent();
         }
 
+        [Authorize]
         [HttpGet("roles/users/{userId}")]
-        public async Task<ActionResult> GetUserRoles()
+        public async Task<ActionResult> GetUserRoles(string userId)
         {
-            // Logic to retrieve roles for a specific user
-            return Ok();
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user is null) return NotFound("User not found.");
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok(roles);
         }
 
+        [Authorize]
         [HttpDelete("roles/users/{userId}")]
-        public async Task<ActionResult> UnAssignUserRoles()
+        public async Task<ActionResult> UnAssignUserRoles([FromBody] IEnumerable<string> roles, string userId)
         {
-            return Ok();
+            var userIdOfRequester = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrWhiteSpace(userIdOfRequester)) return BadRequest("Jwt Name Identifier is null.");
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user is null) return NotFound("User not found.");
+
+            foreach (var role in roles)
+            {
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    return NotFound($"{role} dose not exist.");
+                }
+
+                if (!await _userManager.IsInRoleAsync(user, role))
+                {
+                    return BadRequest($"User is not in role {role}");
+                }
+            }
+
+            var audit = new SecurityAudit
+            {
+                Event = SecurityAuditEvent.Authorization,
+                UserId = userIdOfRequester,
+                Details = $"User changed roles of another user of ID {userId}",
+                Severity = SecurityAuditSeverity.Medium,
+            };
+
+            await _securityAuditStore.SetSecurityAudit(audit);
+
+            await _userManager.RemoveFromRolesAsync(user, roles);
+
+            _logger.LogInformation("User {userId} roles are have been changed to exclude {roles} by User {userIdOfChanger}",
+                                    userId, roles, userIdOfRequester);
+
+            return NoContent();
         }
 
         [HttpGet("roles")]
         public async Task<ActionResult> GetRoles()
         {
-            return Ok();
+            var roles = await _roleManager.Roles.ToListAsync();
+
+            return Ok(roles);
         }
 
         [Authorize]
