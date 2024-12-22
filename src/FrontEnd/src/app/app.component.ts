64f180dc69cb470fbda42d30a819e8f8b7997d6b
Yousaf Wazir <umbrellaman612@gmail.com>
2024-12-22 16:07:12 +0000

    added ScreenService

--- src/FrontEnd/src/app/app.component.ts
+++ src/FrontEnd/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MediaDevicesService } from '../core/browser/media/services/mediaDevices.service';
 import { WebGLService } from '../core/browser/webGL/services/webGL.service';
+import { ScreenService } from '../core/browser/window/services/screen.service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,8 @@ import { WebGLService } from '../core/browser/webGL/services/webGL.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
+  val:any = null
+  constructor(private ss : ScreenService){
+    this.val = ss.getScreenInfo()
+  }
 }
