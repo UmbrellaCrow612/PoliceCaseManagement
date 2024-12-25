aa4997c6fac84b93e32a3a46b0e5a8540567e872
Yousaf Wazir <umbrellaman612@gmail.com>
2024-12-25 17:29:58 +0000

    Update redirect.interceptor.ts

--- src/FrontEnd/src/core/authentication/interceptors/redirect.interceptor.ts
+++ src/FrontEnd/src/core/authentication/interceptors/redirect.interceptor.ts
@@ -1,21 +1,37 @@
-import { HttpErrorResponse, HttpEvent, HttpEventType, HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { catchError, Observable, tap, throwError } from "rxjs";
-import { StatusCodes } from "../../http/codes/status-codes";
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpRequest,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { StatusCodes } from '../../http/codes/status-codes';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
-export function authRedirectsInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+export function authRedirectsInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  var router = inject(Router);
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
+      console.log('RedirectsInterceptor Error response:', error.error);
 
-      if (error.status === StatusCodes.UNAUTHORIZED) {
-        console.log(`Redirecting to lo9gin page`);
+      if (
+        error.status === StatusCodes.UNAUTHORIZED ||
+        error.status === StatusCodes.FORBIDDEN
+      ) {
+        const redirectUrl = error.error?.redirectUrl;
+
+        if (typeof redirectUrl === 'string') {
+          console.log(`Redirecting to ${redirectUrl}`);
+          router.navigate([redirectUrl]);
+        } else {
+          console.error('Invalid redirect URL:', redirectUrl);
+        }
         return throwError(() => error);
       }
-
-      if (error.status === StatusCodes.FORBIDDEN) {
-        console.log(`Redirecting to ${error.error.redirectUrl}`);
-        return throwError(() => error);
-      }
-
       return throwError(() => error);
     })
   );
