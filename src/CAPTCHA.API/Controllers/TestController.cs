2cfdc61f3e5aaf3832b79ec81fff12dbbe95f692
Yousaf Wazir <umbrellaman612@gmail.com>
2025-01-05 11:52:37 +0000

    added Scalar.AspNetCore for UI

---
+++ src/CAPTCHA.API/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using CAPTCHA.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAPTCHA.API.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Test()
+        {
+            var test = CAPTCHAMathService.CreateQuestion();
+
+            return Ok();
+        }
+    }
+}
--- src/CAPTCHA.Core/Services/CAPTCHAMathService.cs
+++ src/CAPTCHA.Core/Services/CAPTCHAMathService.cs
@@ -1,13 +1,36 @@
 ﻿using CAPTCHA.Core.Models;
-
 namespace CAPTCHA.Core.Services
 {
     public class CAPTCHAMathService
     {
-        public (byte[] bytes, CAPTCHAMathQuestion question) CreateQuestion()
+        public static (byte[] imgQuestionBytes, CAPTCHAMathQuestion question) CreateQuestion()
         {
-            throw new NotImplementedException();
+            var random = new Random();
+            int num1 = random.Next(1, 50); 
+            int num2 = random.Next(1, 50); 
+            char[] operations = { '+', '-', '*', '/' };
+            char operation = operations[random.Next(operations.Length)];
+
+            string expression = $"{num1} {operation} {num2}";
+
+            double answer = operation switch
+            {
+                '+' => num1 + num2,
+                '-' => num1 - num2,
+                '*' => num1 * num2,
+                '/' => Math.Round((double)num1 / num2, 2),
+                _ => throw new InvalidOperationException("Unsupported operation")
+            };
+
+            var question = new CAPTCHAMathQuestion
+            {
+                Answer = answer.ToString(),
+                ExpiresAt = DateTime.UtcNow.AddMinutes(2)
+            };
+
+            return ([], question);
         }
+
 
         public bool VaidateQuestion(CAPTCHAMathQuestion question)
         {
