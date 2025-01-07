ae71e67f7e6214c7275759ae29d50287f9b1626b
Yousaf Wazir <umbrellaman612@gmail.com>
2025-01-07 10:03:16 +0000

    make math questions easier

--- src/CAPTCHA.Core/Services/CAPTCHAMathService.cs
+++ src/CAPTCHA.Core/Services/CAPTCHAMathService.cs
@@ -6,21 +6,17 @@ namespace CAPTCHA.Core.Services
         public static (byte[] imgQuestionBytes, CAPTCHAMathQuestion question) CreateQuestion()
         {
             var random = new Random();
-            int num1 = random.Next(1, 50);
-            int num2 = random.Next(1, 50);
-            char[] operations = { '+', '-', '*', '/' };
+            int num1 = random.Next(1, 20); 
+            int num2 = random.Next(1, 20);
+            char[] operations = { '+', '-' };
             char operation = operations[random.Next(operations.Length)];
             string expression = $"{num1} {operation} {num2}";
             double answer = operation switch
             {
                 '+' => num1 + num2,
                 '-' => num1 - num2,
-                '*' => num1 * num2,
-                '/' => Math.Round((double)num1 / num2, 2),
                 _ => throw new InvalidOperationException("Unsupported operation")
             };
-
-            answer = Math.Round(answer, 1);
 
             var question = new CAPTCHAMathQuestion
             {
@@ -33,5 +29,6 @@ namespace CAPTCHA.Core.Services
 
             return (bytes, question);
         }
+
     }
 }
