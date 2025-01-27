ca6d6c151f0d4889b5d39c9185edcea8a40c82c1
Yousaf Wazir <umbrellaman612@gmail.com>
2025-01-27 14:15:00 +0000

    Identity Mobile App clean up

---
+++ src/identity/Identity.Mobile.App/app/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function OTPTab() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});
