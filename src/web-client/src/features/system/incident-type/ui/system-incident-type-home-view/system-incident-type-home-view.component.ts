4cb779c029266fb577ec48da25d2a093d05d3d80
Yousaf Wazir <umbrellaman612@gmail.com>
2025-04-15 16:18:22 +0100

    View incident types in system

--- src/web-client/src/features/system/incident-type/ui/system-incident-type-home-view/system-incident-type-home-view.component.ts
+++ src/web-client/src/features/system/incident-type/ui/system-incident-type-home-view/system-incident-type-home-view.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IncidentTypeService } from '../../../../../core/incident-type/services/incident-type-service.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { IncidentType } from '../../../../../core/incident-type/types';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-system-incident-type-home-view',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormField,
+    MatInputModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+  ],
   templateUrl: './system-incident-type-home-view.component.html',
-  styleUrl: './system-incident-type-home-view.component.css'
+  styleUrl: './system-incident-type-home-view.component.css',
 })
-export class SystemIncidentTypeHomeViewComponent {
+export class SystemIncidentTypeHomeViewComponent implements OnInit {
+  constructor(private incidentTypeService: IncidentTypeService) {}
 
+  ngOnInit(): void {
+    this.fetchData();
+
+    this.searchForm.valueChanges.subscribe(() => {
+      const nameSearchTerm = this.searchForm.controls.name
+        .getRawValue()
+        ?.toLowerCase();
+      const descriptionSearchTerm = this.searchForm.controls.description
+        .getRawValue()
+        ?.toLowerCase();
+
+      this.filteredIncidentTypes = this.incidentTypes.filter((x) => {
+        const matchesName = nameSearchTerm
+          ? x.name?.toLowerCase().includes(nameSearchTerm)
+          : true;
+
+        const matchesDescription = descriptionSearchTerm
+          ? x.description?.toLowerCase().includes(descriptionSearchTerm)
+          : true;
+
+        return matchesName && matchesDescription;
+      });
+    });
+  }
+
+  isLoading = true;
+  errorMessage: string | null = null;
+
+  searchForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  incidentTypes: IncidentType[] = [];
+  filteredIncidentTypes: IncidentType[] = [];
+
+  fetchData() {
+    this.isLoading = true;
+    this.errorMessage = null;
+
+    this.incidentTypeService.getAllIncidentTypes().subscribe({
+      next: (incidentTypes) => {
+        this.incidentTypes = incidentTypes;
+        this.filteredIncidentTypes = incidentTypes;
+        this.isLoading = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.errorMessage = `Error: ${err.error[0]?.code}`;
+        this.isLoading = false;
+      },
+    });
+  }
 }
